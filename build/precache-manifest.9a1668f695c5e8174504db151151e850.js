20c2b0079bdb0ae11a5cf4cb979fc978cd220512
Saambell1996 <Saambell1996@gmail.com>
2019-04-04 18:31:31 +0100

    website
    website

---
+++ build/precache-manifest.9a1668f695c5e8174504db151151e850.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dfb41f097f54fcfbd932c95d32b641",
+    "url": "/webportfolio/static/media/pixelcitypink.49dfb41f.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/webportfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3dcd0c7e240edfe62815",
+    "url": "/webportfolio/static/js/runtime~main.8db7738c.js"
+  },
+  {
+    "revision": "04f396d379c0ec67af02",
+    "url": "/webportfolio/static/js/main.255a5eaf.chunk.js"
+  },
+  {
+    "revision": "a4cc72d8ae7de7c59ca4",
+    "url": "/webportfolio/static/js/2.0ace76f5.chunk.js"
+  },
+  {
+    "revision": "04f396d379c0ec67af02",
+    "url": "/webportfolio/static/css/main.637af74b.chunk.css"
+  },
+  {
+    "revision": "a4cc72d8ae7de7c59ca4",
+    "url": "/webportfolio/static/css/2.24948d6f.chunk.css"
+  },
+  {
+    "revision": "64fec4d612aeb8f969e567873c8e7660",
+    "url": "/webportfolio/index.html"
+  }
+];
